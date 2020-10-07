386d7c7598574e84e0270d93e30664b282fbad81
stepin104509 <69111676+stepin104509@users.noreply.github.com>
2020-10-07 10:24:18 +0530

    Update main.c

--- sourcecode/main.c
+++ sourcecode/main.c
@@ -1,16 +1,17 @@
 void main()
 {
 char a[20];
-int price,Gst=0,total=0;
-clrscr();
+int price,Gst=0,total=0,n;
 printf("\n Billing software for Coffee Day -CD \n");
 printf("\n -------------------------------\n");
 printf("\nEnter the Name of the Item :");
-scanf("%s",&a);
+scanf("%s",a);
+printf("\nEnter the No of Items :");
+scanf("%d",&n);
 printf("\nEnter the Price of the Item:");
 scanf("%d",&price);
 Gst=((price/100)*18);
-total=price*Gst;
+total=(price+Gst)*n;
 printf("\n\n Coffe Day  \n\n");
 printf("\n -----------------\n");
 printf("\n Item   Price   Gst   Total \n");
