473cac388bac589f5f269c85b6ab9819fc6240b0
stepin104509 <69111676+stepin104509@users.noreply.github.com>
2020-10-07 09:43:29 +0530

    Create main.c

---
+++ sourcecode/main.c
@@ -0,0 +1,21 @@
+void main()
+{
+char a[20];
+int price,Gst=0,total=0;
+clrscr();
+printf("\n Billing software for Coffee Day -CD \n");
+printf("\n -------------------------------\n");
+printf("\nEnter the Name of the Item :");
+scanf("%s",&a);
+printf("\nEnter the Price of the Item:");
+scanf("%d",&price);
+Gst=((price/100)*18);
+total=price*Gst;
+printf("\n\n Coffe Day  \n\n");
+printf("\n -----------------\n");
+printf("\n Item   Price   Gst   Total \n");
+printf("\n %s      %d      %d      %d\n",a,price,Gst,total);
+printf("\n ----------------------------");
+printf("\n Thank You ! do Visit Again\n");
+getch();
+}
